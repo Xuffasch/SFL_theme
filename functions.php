34a62719eed83b9364c9f833378290dde8c2bbde
Xuffasch <tristan.lucas@gmail.com>
2020-06-04 09:49:57 +0200

    Shorten functions name by removing 'distribution'

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * Wordpress theme "SFL_Theme" for SFL Distribution
  */
 if ( ! function_exists( 'sfl_setup' ) ) :
-  function sfldistribution_setup() {
+  function sfl_setup() {
     /**
      * Add default support
     */
@@ -11,12 +11,12 @@ if ( ! function_exists( 'sfl_setup' ) ) :
     add_theme_support( 'post-thumbnails' );
   }
 endif;
-add_action( 'after_setup_theme' , 'sfldistribution_setup' );
+add_action( 'after_setup_theme' , 'sfl_setup' );
 
-function sfldistribution_scripts_and_styles() {
+function sfl_scripts_and_styles() {
   wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
-add_action( 'wp_enqueue_scripts', 'sfldistribution_scripts_and_styles' );
+add_action( 'wp_enqueue_scripts', 'sfl_scripts_and_styles' );
 
 /**
  * Create the main menu
