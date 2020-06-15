277b96175f8de0ee940ea462c8be89bb05de4881
Xuffasch <tristan.lucas@gmail.com>
2020-06-15 22:32:13 +0200

    Register sidebar and add more setup functions

--- functions.php
+++ functions.php
@@ -4,22 +4,36 @@
  */
 if ( ! function_exists( 'sfl_setup' ) ) :
   function sfl_setup() {
-    /**
-     * Add default support
-    */
     add_theme_support( 'automatic-feed-links' );
+    
+    /** Add post thumbnail management in admin */
     add_theme_support( 'post-thumbnails' );
-    add_theme_support( 'menus ');
+    /** Add menus management in admin */
+    add_theme_support( 'menus' );
+    /** Add automatic creation of title tag in the page <head> */
+    add_theme_support( 'title-tag' );
+    /** Add woocommerce functions. Without it woocom. Product custom post type is not recongnized and connected to template files */
+    add_theme_support( 'woocommerce' );
   }
 endif;
 add_action( 'after_theme_setup' , 'sfl_setup' );
 
+/** Declare default post thumbnail image sizes */
+set_post_thumbnail_size(960, 720, true);
+
+/** Define other named sizes for thumbnail image */
+add_image_size('medium', 720, 0, false);
+add_image_size('square', 480, 0, false);
+
+/** Include style.css file to pages. The array could contain other css files style could depend on and should be downloaded before style.css */
 function sfl_scripts_and_styles() {
-  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'style', get_stylesheet_uri(), array(), 1.0 );
 }
 add_action( 'wp_enqueue_scripts', 'sfl_scripts_and_styles' );
 
-/**
- * Create the main menu
- */
+/** Declare menus names */
 require_once( get_template_directory().'/inc/menus.php' );
+
+/** Declare sidebars names */
+require_once( get_template_directory().'inc/sidebars.php' );
+
---
+++ inc/sidebars.php
@@ -0,0 +1,7 @@
+<?php
+register_sidebar( array(
+  /** id is used to reference the sidebar in code*/
+  'id' => 'blog-sidebar',
+  /** name is used to reference the sidebar in WordPress Admin */
+  'name' => 'Capitaine Sidebar',
+));
