89210ada83ae7252a77cf36721e9c6f7c85d8875
Xuffasch <tristan.lucas@gmail.com>
2020-06-19 08:46:55 +0200

    Add youtube video by iframe

--- front-page.php
+++ front-page.php
@@ -5,6 +5,8 @@
         <h2>SFL Distribution vous livre en Ile-de-France</h2>
         <h3>du Lundi au Samedi de 7h à 19h30</h3>
       </div>
+      <iframe width=560 height=315 src="https://www.youtube.com/embed/JRQ-w0VqFek" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="main-video">
+      </iframe>
     </div>
   </section>
   <section class="section-container" id="section-1">
@@ -29,4 +31,18 @@
       <h3 class="footer-header">Tous les droits de reproduction réservés à SFL Distribution</h3>
     </div>
   </footer>
+  <!-- Resize the video when the window resizes 
+        1) mobile portrait landscape
+        2) desktop browser
+  --> 
+  <!-- <script>
+    window.onresize = function () {
+      if (window.screen.width < 1024) {
+        console.log("onsize is used");
+        let video = document.getElementById("main-video");
+        let height = video.width * 315 / 560;
+        video.setAttribute("style", "height:" + height + "px");
+      }
+    }
+  </script> -->
 <?php get_footer(); ?>
