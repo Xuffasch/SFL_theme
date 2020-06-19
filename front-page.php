98a3248ee5c157a2e3200031c578aff03d81c523
Xuffasch <tristan.lucas@gmail.com>
2020-06-19 16:19:55 +0200

    Update front-page and separate css files

--- front-page.php
+++ front-page.php
@@ -14,7 +14,31 @@
     <h1>
       <a href="#section-position-1">Nos services - nos engagements</a>
     </h1>
-
+    <div class="section-articles">
+      <article id="article-1">
+        <h1>Selectionner</h1>
+        <p>Choisissez les produits dont nous vous assurons l'approvisionnement une fois commandée</p>
+      </article>
+      <article id="article-2">
+        <h1>Programmer</h1>
+        <p>Tôt le matin, tous les lundis, en début de soirée</p>
+      </article>
+      <article id="article-3">
+        <h1>Cuisiner</h1>
+        <p>Pour ça on ne sait pas faire ! A vous de jouer...</p>
+      </article>
+    </div>
+    <div class="section-articles-titles">
+      <div id="title-1">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/vegetable.svg" alt="selectionner" class="article-img">
+      </div>
+      <div id="title-2">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/calendar2.svg" alt="selectionner" class="article-img">
+      </div>
+      <div id="title-3">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/cooking2.svg" alt="selectionner" class="article-img">
+      </div>
+    </div>
   </section>
   <section class="section-container" id="section-2">
     <div class="section-position" id="section-position-2"></div>
