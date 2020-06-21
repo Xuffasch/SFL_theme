021f3d1495b65f8ffa2e99ad11ce095edd4ede4f
Xuffasch <tristan.lucas@gmail.com>
2020-06-21 22:26:13 +0200

    Set video iframe size at load and resize

--- front-page.php
+++ front-page.php
@@ -5,7 +5,7 @@
         <h2>SFL Distribution vous livre en Ile-de-France</h2>
         <h3>du Lundi au Samedi de 7h à 19h30</h3>
       </div>
-      <iframe width=560 height=315 src="https://www.youtube.com/embed/JRQ-w0VqFek" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="main-video">
+      <iframe src="https://www.youtube.com/embed/JRQ-w0VqFek" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen id="main-video">
       </iframe>
     </div>
   </section>
@@ -43,7 +43,7 @@
   <section class="section-container" id="section-2">
     <div class="section-position" id="section-position-2"></div>
     <h1>
-      <a href="#section-position-2">Nos produits</a>
+      <a href="#section-position-2">Produits récents</a>
     </h1>
 
     <button id="to-top">
@@ -59,14 +59,27 @@
         1) mobile portrait landscape
         2) desktop browser
   --> 
-  <!-- <script>
-    window.onresize = function () {
-      if (window.screen.width < 1024) {
-        console.log("onsize is used");
-        let video = document.getElementById("main-video");
-        let height = video.width * 315 / 560;
-        video.setAttribute("style", "height:" + height + "px");
-      }
+  <script>
+    window.onload = window.onresize = function () {
+      console.log("onsize is used");
+      console.log("window width : " + window.innerWidth);
+      console.log("window height : " + window.innerHeight);
+      
+      let video = document.getElementById("main-video");  
+      let width = 24;
+      let height = 12;
+
+      width = window.innerWidth < 768 ? parseInt(window.innerWidth * .74) : 530;
+      height = window.innerWidth < 768 ? parseInt(width * 315 / 560) : 315;
+
+      console.log("new video width : " + width);
+      console.log("new video height : " + height);
+
+      video.style.width = width + "px";
+      video.style.height = height + "px";
+
+      console.log("video width : " + video.style.width);
+      console.log("video height : " + video.style.height);
     }
-  </script> -->
+  </script>
 <?php get_footer(); ?>
