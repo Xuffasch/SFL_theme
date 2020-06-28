2bbf58e9c794e3a423455b86864e23fa1fc6572c
Xuffasch <tristan.lucas@gmail.com>
2020-06-28 17:09:56 +0200

    payment services icons and styles

---
+++ parts/9-services-logo.php
@@ -0,0 +1,22 @@
+<div class="content-container" id="services-logo">
+  <div class="payment-method" id="CB">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-cartes-bancaires.svg" alt="paiement par carte bancaire">
+    <!-- <h3>Carte Bancaire</h3> -->
+  </div>
+  <div class="payment-method" id="stripe">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-stripe.svg" alt="paiement par Stripe">
+    <!-- <h3>Stripe</h3> -->
+  </div>
+  <div class="payment-method" id="sans-contact">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-contactless.svg" alt="paiement sans contact">
+    <!-- <h3>Sans contact</h3> -->
+  </div>
+  <div class="payment-method" id="apple-pay">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-apple-pay.svg" alt="paiement par Visa">
+    <!-- <h3>Apple pay</h3> -->
+  </div>
+  <div class="payment-method" id="paypal">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-paypal2.svg" alt="paiement par Visa">
+    <!-- <h3>Paypal</h3> -->
+  </div>
+</div>
