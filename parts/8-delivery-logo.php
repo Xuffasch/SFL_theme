75e2af0e8b38222f90b07aaec67a29f3fad2c834
Xuffasch <tristan.lucas@gmail.com>
2020-07-01 17:46:36 +0200

    Create delivery services logo layout

---
+++ parts/8-delivery-logo.php
@@ -0,0 +1,17 @@
+<div class="content-container" id="delivery-logo">
+  <div class="delivery-method" id="deliveroo">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery-deliveroo.svg" alt="livraison par Delivroo">
+  </div>
+  <div class="delivery-method" id="glovo">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery-glovo.svg" alt="livraison par Glovo">
+  </div>
+  <div class="delivery-method" id="just-eat">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery-just-eat.svg" alt="livraison par Just Eat">
+  </div>
+  <!-- <div class="delivery-method" id="uber">
+    <img src="<--?php echo get_template_directory_uri(); ?>/images/delivery-uber.svg" alt="livraison par Uber">
+  </div> -->
+  <div class="delivery-method" id="stuart">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery-stuart.svg" alt="livraison par Stuart">
+  </div>
+</div>
