bb5ef5f1e55fbd9d57d2847ae87f0d06f0a86959
Xuffasch <tristan.lucas@gmail.com>
2020-07-01 17:48:47 +0200

    Set new services section layout

--- parts/10-services.php
+++ parts/10-services.php
@@ -1,33 +1,33 @@
 <div class="content-container" id="services">
   <section class="section-container" id="section-services">
     <div class="section-position" id="position-services"></div>
-    <h1 class="section-title" id="services">
-      <a href="#position-services">Services et engagements</a>
-    </h1>
-    <div class="services-articles">
-      <article id="article-selectionner">
-        <h1>Selectionner</h1>
-        <p>Choisissez les produits dont nous vous assurons l'approvisionnement une fois commandée</p>
-      </article>
-      <article id="article-programmer">
-        <h1>Programmer</h1>
-        <p>Tôt le matin, tous les lundis, en début de soirée</p>
-      </article>
-      <article id="article-cuisiner">
-        <h1>Cuisiner</h1>
-        <p>Pour ça on ne sait pas faire ! A vous de jouer...</p>
-      </article>
-    </div>
-    <div class="services-titles">
-      <div id="title-1">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/vegetable.svg" alt="selectionner" class="article-img">
-      </div>
-      <div id="title-2">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/calendar2.svg" alt="selectionner" class="article-img">
-      </div>
-      <div id="title-3">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/cooking2.svg" alt="selectionner" class="article-img">
+    <div class="section-header">
+      <h1 class="flex-item section-title" id="services">
+        <a href="#position-services" class="mobile">Programmer le date ou la régularité de vos commandes</a>
+        <a href="#position-services" class="desktop">En magasin ou depuis chez vous, votre mode de réglement et de livraison</a>
+      </h1>
+      <div class="flex-item order">
+          <button id="services-btn-order">Commander</button>
       </div>
     </div>
+    <div class="services-icons">
+      <div class="grid-icons">
+        <div class="service-type" id="payment">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/horizontal-line.svg" alt="">
+          <h3> Paiement </h3>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/horizontal-line.svg" alt="">
+        </div>
+        <?php get_template_part("/parts/9-payment-logo" ) ?>
+      </div>
+      <div class="grid-icons">
+        <div class="service-type" id="delivery">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/horizontal-line.svg" alt="">
+          <h3> Livraison </h3>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/horizontal-line.svg" alt="">
+        </div>
+        <?php get_template_part("/parts/8-delivery-logo" ) ?>
+      </div>
+    </div>  
   </section>
 </div>
+
