c2719cfcaefb2bc142351cd7d516ea2d7ae8d1b7
Xuffasch <tristan.lucas@gmail.com>
2020-07-02 08:59:32 +0200

    Responsive ordering of front page sections

--- front-page.php
+++ front-page.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
-  <?php get_template_part('parts/0-landing-video') ?>
-  <?php get_template_part('parts/10-services') ?>
-  <?php get_template_part('parts/20-products') ?>
-  <?php get_template_part('parts/30-hours') ?>
-  <?php get_template_part('parts/99-legal') ?>
+  <div class="container-grid" id="front-page">
+    <?php get_template_part('parts/0-landing-video') ?>
+    <?php get_template_part('parts/10-services') ?>
+    <?php get_template_part('parts/20-products') ?>
+    <?php get_template_part('parts/30-hours') ?>
+    <?php get_template_part('parts/99-legal') ?>
+  </div>
 <?php get_footer(); ?>
