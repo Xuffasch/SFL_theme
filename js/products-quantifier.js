7f38f4f6800db036d81c4f9cf31bf6d4d9e3a649
Xuffasch <tristan.lucas@gmail.com>
2020-07-17 14:09:34 +0200

    Separate functions for Cart add & remove buttons

--- js/products-quantifier.js
+++ js/products-quantifier.js
@@ -1,77 +1,90 @@
 jQuery(document).ready(function() {
-    jQuery(".less").click(function() {
-        console.log("Less button is click for item for" + this.id);
-        let counter = jQuery('.counter-' + this.id)[0];
+    jQuery(".more").click(addQuantity);
+    jQuery(".less").click(removeQuantity);
+})
 
-        if (counter.innerText == "0") {
-            jQuery("#messages").text("Please add a quantity first");
-            return;
+let addQuantity = function() {
+    console.log("addQuantity function is called for product id " + this.id);
+
+    let counterId = jQuery('.counter-' + this.id)[0].id;
+
+    let counter = jQuery('.counter-' + this.id)[0];
+    let counterNumber = counter.innerText;
+    let newQuantity = parseInt(counterNumber) + 1;
+
+    console.log("product_id : " + this.id);
+    console.log("counterId : " + counterId);
+    console.log("newQuantity : " + newQuantity);
+
+    jQuery.ajax({
+        type: "POST",
+        url: ajaxurl,
+        data: {
+            action: "add_one_quantity",
+            product_id: this.id,
+            cart_item_id: counterId,
+            new_quantity: newQuantity
+        },
+        success: function(output) {
+            jQuery("#messages").text(output.data.success);
+
+            if (jQuery("#" + output.data.itemId).length == 0) {
+                jQuery(".counter-" + output.data.productId).attr("id", output.data.itemId);
+            }
+            jQuery("#" + output.data.itemId).text(output.data.newQty);
+
+            jQuery("#" + output.data.itemId + ".result-item").text(output.data.newQty)
+        },
+        error: function(output) {
+            console.log("problème server")
+            jQuery("#messages").text(output.failure);
         }
+    });
+}
 
-        let counterId = jQuery('.counter-' + this.id)[0].id;
-        let counterNumber = counter.innerText;
-        let newQuantity = parseInt(counterNumber) - 1;
+let removeQuantity = function() {
+    console.log("removeQuantity is called for item for" + this.id);
+    let counter = jQuery('.counter-' + this.id)[0];
 
-        console.log("product_id : " + this.id);
-        console.log("counterId : " + counterId);
-        console.log("newQuantity : " + newQuantity);
+    if (counter.innerText == "0") {
+        jQuery("#messages").text("Please add a quantity first");
+        return;
+    }
 
-        jQuery.ajax({
-            type: "POST",
-            url: ajaxurl,
-            data: {
-                action: "remove_one_quantity",
-                product_id: this.id,
-                cart_item_id: counterId,
-                new_quantity: newQuantity
-            },
-            success: function(output) {
-                jQuery("#messages").text("Quantité diminuée");
-                jQuery("#" + output.data.itemId).text(output.data.newQty);
-            },
-            error: function(output) {
-                console.log("problème server")
-                jQuery("#messages").text("Modification non prise en compte - problème connexion server");
+    let counterId = jQuery('.counter-' + this.id)[0].id;
+    let counterNumber = counter.innerText;
+    let newQuantity = parseInt(counterNumber) - 1;
+
+    console.log("product_id : " + this.id);
+    console.log("counterId : " + counterId);
+    console.log("newQuantity : " + newQuantity);
+
+    jQuery.ajax({
+        type: "POST",
+        url: ajaxurl,
+        data: {
+            action: "remove_one_quantity",
+            product_id: this.id,
+            cart_item_id: counterId,
+            new_quantity: newQuantity
+        },
+        success: function(output) {
+            jQuery("#messages").text("Quantité diminuée");
+
+            let listItem = jQuery("#" + output.data.itemId);
+            let resultItem = jQuery("#" + output.data.itemId + ".result-item");
+
+            listItem.text(output.data.newQty);
+            resultItem.text(output.data.newQty);
+
+            if (output.data.newQty == 0) {
+                listItem.removeAttr("id");
+                resultItem.removeAttr("id");
             }
-        });
-    })
-});
-
-jQuery(document).ready(function() {
-    jQuery(".more").click(function() {
-        console.log("More button is click for product id " + this.id);
-
-        let counterId = jQuery('.counter-' + this.id)[0].id;
-
-        let counter = jQuery('.counter-' + this.id)[0];
-        let counterNumber = counter.innerText;
-        let newQuantity = parseInt(counterNumber) + 1;
-
-        console.log("product_id : " + this.id);
-        console.log("counterId : " + counterId);
-        console.log("newQuantity : " + newQuantity);
-
-        jQuery.ajax({
-            type: "POST",
-            url: ajaxurl,
-            data: {
-                action: "add_one_quantity",
-                product_id: this.id,
-                cart_item_id: counterId,
-                new_quantity: newQuantity
-            },
-            success: function(output) {
-                jQuery("#messages").text(output.data.success);
-
-                if (jQuery("#" + output.data.itemId).length == 0) {
-                    jQuery(".counter-" + output.data.productId).attr("id", output.data.itemId);
-                }
-                jQuery("#" + output.data.itemId).text(output.data.newQty);
-            },
-            error: function(output) {
-                console.log("problème server")
-                jQuery("#messages").text(output.failure);
-            }
-        });
-    })
-});
+        },
+        error: function(output) {
+            console.log("problème server")
+            jQuery("#messages").text("Modification non prise en compte - problème connexion server");
+        }
+    });
+}
