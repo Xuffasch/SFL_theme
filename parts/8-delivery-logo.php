8447c98827ffce9bc314af39de2b0e32f019b39f
Xuffasch <tristan.lucas@gmail.com>
2020-07-24 17:48:41 +0200

    Change image directory

--- parts/8-delivery-logo.php
+++ parts/8-delivery-logo.php
@@ -1,17 +1,17 @@
 <div class="content-container" id="delivery-logo">
   <div class="delivery-method" id="deliveroo">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery-deliveroo.svg" alt="livraison par Delivroo">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery/delivery-deliveroo.svg" alt="livraison par Delivroo">
   </div>
   <div class="delivery-method" id="glovo">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery-glovo.svg" alt="livraison par Glovo">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery/delivery-glovo.svg" alt="livraison par Glovo">
   </div>
   <div class="delivery-method" id="just-eat">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery-just-eat.svg" alt="livraison par Just Eat">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery/delivery-just-eat.svg" alt="livraison par Just Eat">
   </div>
   <!-- <div class="delivery-method" id="uber">
     <img src="<--?php echo get_template_directory_uri(); ?>/images/delivery-uber.svg" alt="livraison par Uber">
   </div> -->
   <div class="delivery-method" id="stuart">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery-stuart.svg" alt="livraison par Stuart">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/delivery/delivery-stuart.svg" alt="livraison par Stuart">
   </div>
 </div>
--- parts/9-payment-logo.php
+++ parts/9-payment-logo.php
@@ -1,22 +1,22 @@
 <div class="content-container" id="payment-logo">
   <div class="payment-method" id="CB">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-cartes-bancaires.svg" alt="paiement par carte bancaire">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment/payment-cartes-bancaires.svg" alt="paiement par carte bancaire">
     <!-- <h3>Carte Bancaire</h3> -->
   </div>
   <div class="payment-method" id="stripe">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-stripe.svg" alt="paiement par Stripe">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment/payment-stripe.svg" alt="paiement par Stripe">
     <!-- <h3>Stripe</h3> -->
   </div>
   <div class="payment-method" id="sans-contact">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-contactless.svg" alt="paiement sans contact">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment/payment-contactless.svg" alt="paiement sans contact">
     <!-- <h3>Sans contact</h3> -->
   </div>
   <div class="payment-method" id="apple-pay">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-apple-pay.svg" alt="paiement par Visa">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment/payment-apple-pay.svg" alt="paiement par Visa">
     <!-- <h3>Apple pay</h3> -->
   </div>
   <div class="payment-method" id="paypal">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/payment-paypal2.svg" alt="paiement par Visa">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/payment/payment-paypal2.svg" alt="paiement par Visa">
     <!-- <h3>Paypal</h3> -->
   </div>
 </div>
