e589cec529ff26ac12b15390317f3cd0dae128e5
Xuffasch <tristan.lucas@gmail.com>
2020-07-29 10:08:31 +0200

    Improve cart button click response with background

--- functions/custom-primary-menu.php
+++ functions/custom-primary-menu.php
@@ -33,14 +33,25 @@ function custom_primary_menu() {
         $navBar .= '<a href="'.get_permalink( wc_get_page_id( 'myaccount' )).'" alt="Account" class="user-menu-item menu-icon'.$logged.'" id="my-account">'.$icon.'</a>';
         break;
       case 227:
+        $cart_background_start = '<a href="'.get_permalink( wc_get_page_id( 'cart' )).'" alt="Account" class="menu-icon" id="my-account"><div class="cart-background-filler">'; 
+        $cart_background_end = '</div></a>';
+        
         $icon = file_get_contents(get_template_directory()."/images/products/basket-round.svg");
-        $cart_counter = "<div class='cart-item-counter'><h2 id='cart-counter'>".count(WC()->cart->get_cart())."</h2></div>";
+        $cart_icon = '<div class="cart-icon">'.$icon.'</div>';
+        
+        $cart_counter = '<div class="cart-item-counter"><h2 id="cart-counter">'.count(WC()->cart->get_cart()).'</h2></div>';
+
         $in_cart = is_cart() ? 'invisible' : '';
-        $navBar .= '<div class="user-menu-item cart-counter '.$in_cart.'"><a href="'.get_permalink( wc_get_page_id( 'cart' )).'" alt="Account" class="menu-icon" id="my-account">'.$icon.'</a>'.$cart_counter.'</div>';
+        
+        $navBar .= '<div class="user-menu-item cart-counter '.$in_cart.'">';
+        $navBar .= $cart_background_start;
+        $navBar .= $cart_icon;
+        $navBar .= $cart_counter;
+        $navBar .= $cart_background_end;
         $navBar .= '</div>';
         break;
     }
   }
-  
+
   echo $navBar;
 }
